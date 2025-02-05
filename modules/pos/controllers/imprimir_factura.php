135bd455f369192897e941f948c031e4bef11453
JohanRengifo <johanstivenrengifo@outlook.com>
2025-02-05 16:36:50 -0500

    Fix Bugs #25

--- modules/pos/controllers/imprimir_factura.php
+++ modules/pos/controllers/imprimir_factura.php
@@ -226,9 +226,9 @@ try {
 
     // Espacios para firmas
     $pdf->Ln(20);
-    $pdf->Cell(95, 0, '', 'T', 0, 'C');
-    $pdf->Cell(20, 0, '', 0, 0);
-    $pdf->Cell(95, 0, '', 'T', 1, 'C');
+    $pdf->Cell(80, 0, '', 'T', 0, 'C');
+    $pdf->Cell(10 , 0, '', 0, 0);
+    $pdf->Cell(80, 0, '', 'T', 1, 'C');
     
     $pdf->SetFont('Arial', '', 10);
     $pdf->Cell(95, 5, 'VENDEDOR', 0, 0, 'C');
